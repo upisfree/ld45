492f8a3d3f1ff654c97b351511258c9912041d17
Senya Pugach <upisfree@artlebedev.ru>
2019-09-28 18:22:12 +0300

    simple npc collisions

--- src/game/npc.ts
+++ src/game/npc.ts
@@ -18,12 +18,17 @@ class NPC extends Sprite {
   }
 
   update() {
+    let collisionVector = new Vector2(
+      this.position.x + (this.moveSpeed + 0.75) * Math.cos(this.rotation),
+      this.position.y + (this.moveSpeed + 0.75) * Math.sin(this.rotation)
+    );
+
     let v = new Vector2(
       this.position.x + this.moveSpeed * Math.cos(this.rotation),
       this.position.y + this.moveSpeed * Math.sin(this.rotation)
     );
 
-    let t = this.level.getWallType(v);
+    let t = this.level.getWallType(collisionVector);
 
     if (this.collisionWith || !Level.isWallTypeVoidOrAir(t)) {
       this.rotation += Math.random() * Math.PI * 2;
